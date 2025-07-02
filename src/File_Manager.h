e5fe123b68c79078c804b492b934e57fe071b8f8
jorgeerazo1010 <00047225@uca.edu.sv>
2025-07-02 15:00:20 -0600

    Correccion de FileManager, se agrego el .cpp donde se hace llamado a la libreria

--- src/File_Manager.h
+++ src/File_Manager.h
@@ -4,17 +4,7 @@
 #include <string>
 #include <vector>
 
-bool readFileLines(const std::string &path, std::vector<std::string> &lines)
-{
-    std::ifstream in(path);
-    if (!in.is_open())
-        return false;
-    std::string line;
-    while (std::getline(in, line))
-        lines.push_back(line);
-    in.close();
-    return true;
-    bool writeFileLines(const std::string &path, const std::vector<std::string> &lines);
-}
+bool readFileLines(const std::string &path, std::vector<std::string> &lines);
+bool writeFileLines(const std::string &path, const std::vector<std::string> &lines);
 
 #endif
