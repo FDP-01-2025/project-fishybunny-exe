cd89cb5ac9d98323e65aa8fdd4788a790b4dca33
CarlosFlores04 <00079225@uca.edu.sv>
2025-07-02 16:25:40 -0600

    correccion de errores y adicion de funciones en el character manager

--- src/CharacterManager.cpp
+++ src/CharacterManager.cpp
@@ -1,5 +1,5 @@
 #include "CharacterManager.h" //incluimos las librerias creadas
-#include "FileManager.h"
+#include "File_Manager.h"
 #include "Utils.h"
 #include <iostream>
 #include <vector>
@@ -42,15 +42,64 @@ void loadCharacters(const string &user)
     }
 }
 // guarda los personajes del usuario actual en un archivo de texto
-void saveCharacters(const std::string &user)
+void saveCharacters(const string &user)
 {
-    std::vector<std::string> lines;
+    vector<string> lines;
     for (auto &c : chars)
     {
         // convierte cada personaje a una linea de texto separada
         lines.push_back(
             c.name + "," +
-            std::to_string(c.level) + "," +
-            std::to_string(c.typeID) + "," +
-            std::to_string(c.exp));
+            to_string(c.level) + "," +
+            to_string(c.typeID) + "," +
+            to_string(c.exp));
     }
+    writeFileLines(user + "_chars.txt", lines);
+}
+//creamos a la funcion para que nos muestre los personajes
+void showCharacters()
+{
+    cout << "\nYour Characters:\n";
+    if (chars.empty())
+    {
+        cout << "  (none)\n";
+        return;
+    }
+    for (size_t i = 0; i < chars.size(); ++i)
+    {
+        auto &c = chars[i];
+        cout
+            << "  " << i + 1 << ") " << c.name
+            << " (Type " << c.typeID
+            << ") Lv." << c.level
+            << " Exp:" << c.exp
+            << "\n";
+    }
+}
+//creamos a la funcion de la eleccion de personajes
+int selectCharacter()
+{
+    if (chars.empty())
+        return -1;
+    showCharacters();
+    cout << "Select # or 0 to cancel: ";
+    int c = getIntInput(0, (int)chars.size());
+    return c ? c - 1 : -1;
+}
+//creamos la funcion para que el usuario cree sus propios personajes
+int createCharacter(const string &name, int typeID)
+{
+    if (chars.size() >= MAX_CH)
+    {
+        cout << "Max 5 characters. Delete one first.\n";
+        return -1;
+    }
+    chars.push_back({name, 1, typeID, 0});
+    saveCharacters(curUser);
+    return (int)chars.size() - 1;
+}
+//empezamos a crear la funcion para que el usuario pueda borrar a sus personajes
+void deleteCharacter(int idx)
+{
+    if (idx >= 0 && idx < (int)chars.size())
+    {
